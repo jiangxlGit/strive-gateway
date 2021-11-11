01f53d387bbc5df8997431199e011e2748949310
jiangxl <jiangxinlin@biaopu.cloud>
2021-11-11 16:58:38 +0800

    完善knife4j

--- src/main/java/com/jloved/strive/gateway/configuration/SwaggerProvider.java
+++ src/main/java/com/jloved/strive/gateway/configuration/SwaggerProvider.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.config.GatewayProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.support.NameUtils;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import springfox.documentation.swagger.web.SwaggerResource;
@@ -35,25 +36,16 @@ public class SwaggerProvider implements SwaggerResourcesProvider {
   @Override
   public List<SwaggerResource> get() {
     List<SwaggerResource> resources = new ArrayList<>();
-    List<String> routeHosts = new ArrayList<>();
-    // 由于我的网关采用的是负载均衡的方式，因此我需要拿到所有应用的serviceId
-    // 获取所有可用的host：serviceId
-    routeLocator.getRoutes().filter(route -> route.getUri().getHost() != null)
-        .filter(route -> !self.equals(route.getUri().getHost()))
-        .subscribe(route -> routeHosts.add(route.getUri().getHost()));
-
-    // 记录已经添加过的server，存在同一个应用注册了多个服务在nacos上
-    Set<String> dealed = new HashSet<>();
-    routeHosts.forEach(instance -> {
-      // 拼接url，样式为/serviceId/v2/api-info，当网关调用这个接口时，会自动通过负载均衡寻找对应的主机
-      String url = "/" + instance + API_URI;
-      if (!dealed.contains(url)) {
-        dealed.add(url);
-        SwaggerResource swaggerResource = new SwaggerResource();
-        swaggerResource.setUrl(url);
-        swaggerResource.setName(instance);
-        resources.add(swaggerResource);
-      }
+    List<String> routes = new ArrayList<>();
+    //获取所有路由的ID
+    routeLocator.getRoutes().subscribe(route -> routes.add(route.getId()));
+    //过滤出配置文件中定义的路由->过滤出Path Route Predicate->根据路径拼接成api-docs路径->生成SwaggerResource
+    gatewayProperties.getRoutes().stream().filter(routeDefinition -> routes.contains(routeDefinition.getId())).forEach(route -> {
+      route.getPredicates().stream()
+          .filter(predicateDefinition -> ("Path").equalsIgnoreCase(predicateDefinition.getName()))
+          .forEach(predicateDefinition -> resources.add(swaggerResource(route.getId(),
+              predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0")
+                  .replace("**", "v2/api-docs"))));
     });
     return resources;
   }
